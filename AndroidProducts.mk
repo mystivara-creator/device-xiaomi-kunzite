b8b3e47d3c96f1807731827bb42d8b23af6e037b
Mystivara <shelentzz3177@gmail.com>
2026-09-17 02:06:10 +0700

    Add AndroidProducts.mk for PixelExperience project

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pixel_kunzite.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixel_kunzite-user \
+    pixel_kunzite-userdebug \
+    pixel_kunzite-eng
+
